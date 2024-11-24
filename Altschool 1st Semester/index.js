516d5cba5ec90c8b2af51b99e83e6f4ba36b8d5b
Johnson Kayode <kayodejohnson01@gmail.com>
2024-11-24 04:22:26 +0100

    learnign javascript

--- Altschool 1st Semester/index.js
+++ Altschool 1st Semester/index.js
@@ -1,12 +1,12 @@
 console.log("Hello world")
 
 
-console.log('Undefined, Null, boolean, string, symbol, number and object') //to print something to the console.
+console.log('Undefined, Null, boolean, string, symbol, number and object'); //to print something to the console.
 
 
 // using and declaring variables Var, let and const to set variable. Consts are unchangeable, while the other 2 can be modified#
 
-var number= 'johnson';
+var numbere= 'johnson';
 let secondnumber= 'kayode emmanuel';
 const justina = 'usual'  //This line can never change 
 
@@ -17,3 +17,45 @@ let number = 7;
 let numbers;
 
 numbers = 22;
+
+console.log('Numbers=', numbers);
+console.log(secondnumber)
+
+//Javascript is case sensitive
+//Numeric work
+let a, b, sum;
+
+a = 10;
+b = 20;
+
+b += 19;
+a -= 12;
+
+a *= 2;
+b *= 32;
+
+a /= 2;
+b /=5;
+
+sum = a + b;
+
+console.log('SUm is:', sum);
+
+//Strings 
+
+let string = "There is nothing\ serious to see here";
+let string2 = `This is what it means "to you a backtick"`;
+
+console.log(string2);
+
+//Escape sequence 
+// \\  backslash
+// \n  newline
+// \t  tab 
+// \b  backspace
+// \f  form feed
+
+// String concatenation
+
+let addedStrings = string + '\t' + '\n' + string2;
+console.log(addedStrings);
