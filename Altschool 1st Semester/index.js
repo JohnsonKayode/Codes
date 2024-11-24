fdaee0434c45b9fd43056e69cd9350ebc2e2c1a3
Johnson Kayode <kayodejohnson01@gmail.com>
2024-11-24 03:42:01 +0100

    updated files

---
+++ Altschool 1st Semester/index.js
@@ -0,0 +1,19 @@
+console.log("Hello world")
+
+
+console.log('Undefined, Null, boolean, string, symbol, number and object') //to print something to the console.
+
+
+// using and declaring variables Var, let and const to set variable. Consts are unchangeable, while the other 2 can be modified#
+
+var number= 'johnson';
+let secondnumber= 'kayode emmanuel';
+const justina = 'usual'  //This line can never change 
+
+//storing variables with assignment variables
+//always add a semi colon at the line of every js code to know where a line of code ends
+
+let number = 7;
+let numbers;
+
+numbers = 22;
